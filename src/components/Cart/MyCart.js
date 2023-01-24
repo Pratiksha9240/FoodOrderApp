ed5d4f59cca0e761e30ffe932b39e0ede8882453
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-01-24 15:01:26 +0530

    Making hard coded cart with createReactportal

---
+++ src/components/Cart/MyCart.js
@@ -0,0 +1,25 @@
+import classes from './MyCart.module.css';
+import Modal from '../UI/Modal';
+
+const MyCart = () => {
+
+    const items = [{id: 1, name: 'Paneer', quantity: 5, price: 20}].map(i => (
+        <li>{i.name}</li>
+    ));
+
+    return(
+        <Modal>
+            {items}
+            <div>
+                <span>Total price</span>
+                <span>12</span>
+            </div>
+            <div>
+                <button className={classes.close}>Close</button>
+                <button className={classes.order}>Order</button>
+            </div>
+        </Modal>
+    )
+}
+
+export default MyCart;
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,31 @@
+import { useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import MyCart from '../Cart/MyCart';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+
+  let flag = false;
+
+  const [f,setFlag] = useState(flag);
+
+  const onClickHandler = () => {
+    setFlag(true);
+  }
+
   return (
-    <button className={classes.button}>
+    <>
+    <button onClick={onClickHandler} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
       <span className={classes.badge}>0</span>
     </button>
+    <div>
+      {f === true && <MyCart/>}
+    </div>
+    </>
   );
 };
 
