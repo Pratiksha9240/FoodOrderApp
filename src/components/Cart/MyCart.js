a58c98e587d763f980e74029f2c13bd37a45509d
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-01-27 13:37:47 +0530

    Displaying items in cart

--- src/components/Cart/MyCart.js
+++ src/components/Cart/MyCart.js
@@ -1,25 +1,42 @@
-import classes from './MyCart.module.css';
-import Modal from '../UI/Modal';
+import classes from "./MyCart.module.css";
+import Modal from "../UI/Modal";
+import { useContext } from "react";
+import CartContext from "../../context/cart-context";
 
 const MyCart = (props) => {
+  const cartCtx = useContext(CartContext);
 
-    const items = [{id: 1, name: 'Paneer', amount: 5, price: 20}].map(i => (
-        <li>{i.name}</li>
-    ));
+  const items = (
+    <ul className={classes.meals}>
+      {cartCtx.items.map((i) => (
+        <>
+          <h2>{i.name}</h2>
+          <p className={classes.p}>${i.price}</p>
+          <span>
+          <button className={classes.add}>+</button>
+          <button className={classes.remove}>-</button>
+          </span>
+          <hr />
+        </>
+      ))}
+    </ul>
+  );
 
-    return(
-        <Modal>
-            {items}
-            <div>
-                <span>Total price</span>
-                <span>12</span>
-            </div>
-            <div>
-                <button className={classes.close} onClick={props.onClose}>Close</button>
-                <button className={classes.order}>Order</button>
-            </div>
-        </Modal>
-    )
-}
+  return (
+    <Modal>
+      {items}
+      <div>
+        <span>Total price</span>
+        <span>12</span>
+      </div>
+      <div>
+        <button className={classes.close} onClick={props.onClose}>
+          Close
+        </button>
+        <button className={classes.order}>Order</button>
+      </div>
+    </Modal>
+  );
+};
 
 export default MyCart;
